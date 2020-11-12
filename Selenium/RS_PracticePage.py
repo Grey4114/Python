977eda3d509bcfa73ae0b6e05a3127decae8c4b4
Grey4114 <60712674+Grey4114@users.noreply.github.com>
2020-11-11 22:01:06 -0800

    Update RS_PracticePage.py

--- Selenium/RS_PracticePage.py
+++ Selenium/RS_PracticePage.py
@@ -23,12 +23,45 @@ driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 
 # RADIO BUTTONS
 # Find the radio buttons and click the 2nd button
-radio_buttons = driver.find_elements_by_name("radioButton")     # Finds all of the radio button elements
-radio_buttons[1].click()    # Clicks on the 2nd button
+radio_buttons = driver.find_elements_by_name("radioButton")
+radio_buttons[1].click()
+
+
+# TODO - COUNTRY TEXT ENTRY FIELD
+# Enter a the beginning of a country
+text_field = driver.find_elements_by_name()
+
+
+# TODO - DROPDOWN MENU
+# Find the dropdown box and select option 2
+# drop_down = driver.find_elements_by_name("dropdown-class-example")
+# drop_down[1].click()
 
 
 
+# TODO - CHECKBOX
+# Find the checkbox and check all 3 options
+check_box = driver.find_elements_by_name()
 
+# TODO - SWITCH WINDOW
+# Find the switch window and select
+switch_window = driver.find_elements_by_name()
+
+# TODO - SWITCH TABS
+# Find the switch tabs and select
+switch_tabs = driver.find_elements_by_name()
+
+# TODO - SWITCH ALERTS
+# Find the switch alerts, enter name and select
+switch_alerts = driver.find_elements_by_name()
+
+
+# TODO - DISPLAY ELEMENTS
+# Find display elements and hide/show
+
+
+# TODO - MOUSE HOVER OVER
+# Find display hover and show
 
 
 
