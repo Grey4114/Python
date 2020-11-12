ed6188458b5529d2ea2f1c70dcef06c9b6c03e60
Grey4114 <60712674+Grey4114@users.noreply.github.com>
2020-11-11 22:12:57 -0800

    Create CoinFlip.py

---
+++ Unittest/CoinFlip/CoinFlip.py
@@ -0,0 +1,36 @@
+"""
+Coin Flip Simulation - Write some code that simulates flipping a single coin however many times the user decides.
+The code should record the outcomes and count the number of tails and heads.
+
+Created: 11/11/2020
+Unittest Script:
+Notes:
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
