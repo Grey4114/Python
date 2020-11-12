156b6699ce74d3ec2d6cd58c5f8896d114aee984
Grey4114 <60712674+Grey4114@users.noreply.github.com>
2020-11-12 10:36:24 -0800

    CoinFlip Structure
    Created directory structure and init.py files

--- Unittest/CoinFlip/CoinFlip.py
+++ Unittest/CoinFlip/CoinFlip.py
@@ -1,13 +1,35 @@
 """
-Coin Flip Simulation - Write some code that simulates flipping a single coin however many times the user decides.
+Coin Flip Simulation
+- Write some code that simulates flipping a single coin however many times the user decides.
 The code should record the outcomes and count the number of tails and heads.
 
-Created: 11/11/2020
-Unittest Script:
+Script : This is the main script
+Created: 11/11/2020 - 11/
+
+Class & Function Script(s):
+Unittest Script(s):
+
 Notes:
 
 """
 
+import random
+
+
+
+# ToDo - Create Global variables
+# ToDo - Creat a coin object
+# ToDo - Create a Player object - Name
+# ToDo - Ask player number of times to flip coin
+# ToDo - Create a random number generator(RNG) - 1 to 10 - odds / evens
+# ToDo - Flip the coin using RNG
+# ToDo - Post coin flip results of each coin as it is flipped
+# ToDo - Count each flip
+# ToDo - Count results of each coin flip - Heads / Tails
+# ToDo - Post total Head / Tails results
+# ToDo - Create main
+
+# ToDo - Break Class and Functions into own scripts
 
 
 
